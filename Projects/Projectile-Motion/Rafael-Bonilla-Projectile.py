886d05192acb8f0035072277f6b4d1188e57ecd0
CSI-Rafael-Bonilla <25-007@sanignacio.pr>
2021-10-05 17:20:00 -0400

    Gun go brrrr

---
+++ Projects/Projectile-Motion/Rafael-Bonilla-Projectile.py
@@ -0,0 +1,13 @@
+OceanTower = int("74.07")
+
+AssaultRifle = "DT MDR 7.62x51"
+
+Bullet = "7.62x51mm Ultra Nosler"
+
+BulletSpeed = 822
+
+BulletWeight = int("0.021")
+
+print("Calculate the time that the bullet would take from OceanTower to the floor")
+
+print({BulletSpeed}/{OceanTower})
