1efc1509e6eaccb705b23d26e21c475f438e84b7
CSI-Rafael-Bonilla <25-007@sanignacio.pr>
2021-10-15 12:51:42 -0400

    Update Rafael-Bonilla-Projectile.py

--- Projects/Projectile-Motion/Rafael-Bonilla-Projectile.py
+++ Projects/Projectile-Motion/Rafael-Bonilla-Projectile.py
@@ -1,6 +1,6 @@
 import math
 
-OceanTower = 74.07
+OceanTowerkm = 74.07
 
 AssaultRifle = "DT MDR 7.62x51"
 
@@ -14,8 +14,8 @@ Gravity = 9.8
 
 print("Calculate the time that the bullet would take from OceanTower to the floor")
 
-Time = math.sqrt((int(OceanTower) * 2) / int(Gravity))
+Time = math.sqrt((int(OceanTowerkm) * 2) / int(Gravity))
 
-Velocity = (OceanTower / Time)
+Velocity = (OceanTowerkm / Time)
 
 delta_x =  (Velocity / Time)
