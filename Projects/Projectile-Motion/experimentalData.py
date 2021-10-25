0242f14132ff5bd4c636f83ef75a030ac1684028
CSI-Rafael-Bonilla <25-007@sanignacio.pr>
2021-10-25 18:11:32 -0400

    Project

--- Projects/Projectile-Motion/experimentalData.py
+++ Projects/Projectile-Motion/experimentalData.py
@@ -1,18 +1,37 @@
+import math
+
 class ExperimentalData:
-    def __init__(self, Gun:str, Calliber:str, Bullet:str, Velocityms:str, Building:int, Buildingheight_m:int, gravityms:int):
+    def __init__(self, Gun:str, Calliber:str, Bullet:str, Velocityms:str, Building:int, Buildingheight_m:int, planet:int):
         self.Gun = Gun
         self.Calliber = Calliber
         self.Bullet = Bullet
         self.Velocityms = Velocityms
         self.Building = Building
         self.BuildingHeight = Buildingheight_m
-        self.gravityms = gravityms
+        self.planet = planets
+        self.g_ms2 = g_ms2
 
-        Gun = "DT MDR 7.62x51"
-        Calliber = "7.62x51mm NATO"
-        Bullet = "7.62x51mm Ultra Nosler"
-        Velocityms = 18.27
-        Building = "Ocean Tower"
-        Buildingheight_m = 74.07
-        gravityms = 9.81
+#Parallel List
+planets = "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+g_ms2 = [3.7, 8.87, 9.81, 3.711, 24.79, 10.44, 8.69, 11.15]
+
+# Planet Index
+planet = planets.index(input("Planet Name: "))
+
+# Planet Gravity
+g_ms2[planet]
+
+def getTime (self):
+    return (math.sqrt((int(ExperimentalData.Building_m) * 2) / int(ExperimentalData.planet)))
+
+#Parallel List
+planets = "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+g_ms2 = [3.7, 8.87, 9.81, 3.711, 24.79, 10.44, 8.69, 11.15]
+
+# Planet Index
+planet = planets.index(input("Planet Name: "))
+
+# Planet Gravity
+g_ms2[planet]
+
 
