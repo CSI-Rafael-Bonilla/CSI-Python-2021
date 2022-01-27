7144ef9757ab4c2087935e519fb81f688ee7674b
CSI-Rafael-Bonilla <25-007@sanignacio.pr>
2022-01-27 11:34:41 -0400

    Webserver

---
+++ Projects/Web-Servers/color.py
@@ -0,0 +1,8 @@
+class Color:
+    def __init__(self, Id:int, Uid:str, HexValue:str, ColorName:str, Hslvalue:int, Hslavalue:int):
+        self.Id = Id
+        self.Uid = Uid
+        self.HexValue = HexValue
+        self.ColorName = ColorName
+        self.Hslvalue = Hslvalue
+        self.Hslavalue = Hslavalue
