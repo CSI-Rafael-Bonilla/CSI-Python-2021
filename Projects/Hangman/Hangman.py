99fc16fd2334d922a68573fb566b452ec79d91be
CSI-Rafael-Bonilla <25-007@sanignacio.pr>
2022-02-18 11:49:53 -0400

    Hangman

--- Projects/Hangman/Hangman.py
+++ Projects/Hangman/Hangman.py
@@ -1,5 +1,6 @@
 from asyncio.windows_events import NULL
 import json, ssl
+from socket import if_indextoname
 from pathlib import Path
 import os
 from urllib import response
@@ -17,6 +18,8 @@ req = urllib.request.Request(colorURL)
 requestData = json.loads(urllib.request.urlopen(req).read())
 
 color:Color = Color(**requestData)
+
+my_List = [""]
 
 steps = ["""
     +----------------+
@@ -101,11 +104,14 @@ print(steps[0])
 
 print (len(color.color_name)* " _")
 
-
-
 def input_function():
     while(True):
         letter = input("Choose letters to guess a word related to a color")
+
+        special_characters = "!@#$%^&*()-+?_=,<>/"
+        s=input()
+
+        
 
         if(len(letter)!= 1):
             print("ERROR input a letter, no more than one character")
@@ -113,6 +119,32 @@ def input_function():
         if (letter.isdigit()):
             print("ERROR input a letter, not a digit")
             continue
-        if  (letter.isalnum()):
+        if (letter.isspace()):
+            print (("ERROR input a letter, not a space"))
+            continue
+        if  (letter in special_characters):
             print("ERROR input a letter, not a special character")
             continue
+
+        my_List.append(letter)
+        return letter
+
+print(input_function())
+
+def printword ():
+    Temp:str=""
+    
+    for letter in color.color_name:
+        if letter in color.color_name:
+            Temp+= letter
+        else: 
+            Temp+="_"
+    print (Temp)
+
+#while (True):
+    #print (steps[0])
+    #get input
+
+def stepcounter():
+    while(True):
+        if my_List.append          
