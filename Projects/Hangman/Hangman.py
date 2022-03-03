f8ce71e61af1c05944d046fef48b2162b5352799
CSI-Rafael-Bonilla <25-007@sanignacio.pr>
2022-03-03 12:10:41 -0400

    hangman

--- Projects/Hangman/Hangman.py
+++ Projects/Hangman/Hangman.py
@@ -2,18 +2,20 @@ import json, ssl
 import urllib.request
 from Color import Color
 
+def getColor():
 # This is discouraged but it will avoid certificate validation (prevents error)
-ssl._create_default_https_context = ssl._create_unverified_context
+    ssl._create_default_https_context = ssl._create_unverified_context
 
 # This is the URL from which we will request the data
-colorURL = "https://random-data-api.com/api/color/random_color"
+    colorURL = "https://random-data-api.com/api/color/random_color"
 
 # Execute HTTP Request
-req = urllib.request.Request(colorURL)
-requestData = json.loads(urllib.request.urlopen(req).read())
+    req = urllib.request.Request(colorURL)
+    requestData = json.loads(urllib.request.urlopen(req).read())
 
 # It enables the objects in the class Color to be used in this code
-color:Color = Color(**requestData)
+    color:Color = Color(**requestData)
+    return color.color_name
 
 # my_List is a list that compiles all of the wrong letters.
 my_List = [""]
@@ -96,12 +98,12 @@ steps = ["""
     |
     |
     \__________________"""]
-print(steps[0])
+# print(steps[0])
 
 #print (color.color_name)
 
 # it prints the amount of letters in the word in underscores, _.
-print (len(color.color_name)* " _")
+# print (len(color.color_name)* " _")
 
 # It validates if the letter chosen is under one character and if it as a letter.
 def input_function():
@@ -127,39 +129,44 @@ def input_function():
         my_List.append(letter)
         return letter
 
-print(input_function())
+# print(input_function())
 
 # This function that prints the word with the letters added by the player, if it was not a part of the letters in the word, then it stays the same. 
-def printword ():
+def printword (color):
     Temp:str=""
-    for letter in color.color_name:
+    for letter in color:
         if letter in my_List:
             Temp+= letter
         else: 
             Temp+="_"
     print (Temp)
 
-def getErrors():
+def getErrors(color):
     # Every time it runs, it detects if a letter that was not in the color name and adds +1 to the amount of errors
     error = 0
 
     for letter in my_List:
-        if (letter not in color.color_name):
+        if (letter not in color):
             error += 1
-    if error == 7:
-        print("GAME OVER, you absolute buffoon the word was")
-        print (color.color_name)
+        
     return error
 
 # After the player answers the first letter, the game restarts the function that validates the letters, the one that prints the words, and if the steps are the same as the amount of errors
+
 while (True):
-    input_function()
-    printword()
-    print (steps[getErrors()]) 
+    color = getColor() 
+    # error = getErrors(color)
+    while (True):
+        error = getErrors(color)
         
-    if my_List == 7:
-        requestData = json.loads(urllib.request.urlopen(req).read())
-        color:Color = Color(**requestData)
-        my_List.clear()
-        print(steps[0])
-        print (len(color.color_name)* " _")
+        print (steps[error])
+        printword(color)
+       
+
+        if error == 6:
+            print("GAME OVER, you absolute buffoon the word was")
+            print (color)
+            print("-------------------------------------------")
+            break
+
+        input_function()
