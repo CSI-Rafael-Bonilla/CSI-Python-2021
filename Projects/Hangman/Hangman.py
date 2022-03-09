a0c4da8bcf9eb0716e8017f5d629bccb72e6074f
CSI-Rafael-Bonilla <25-007@sanignacio.pr>
2022-03-09 11:27:26 -0400

    Hangman

--- Projects/Hangman/Hangman.py
+++ Projects/Hangman/Hangman.py
@@ -15,12 +15,12 @@ def getColor():
 
 # It enables the objects in the class Color to be used in this code
     color:Color = Color(**requestData)
-    return color.color_name
+    return color.color_name.upper()
 
 # my_List is a list that compiles all of the wrong letters.
 my_List = [""]
 
-# steps is the amount of steps that there are until the the player loses.
+# variable "steps" is the amount of steps that there are.
 steps = ["""
     +----------------+
     |                |
@@ -108,11 +108,11 @@ steps = ["""
 # It validates if the letter chosen is under one character and if it as a letter.
 def input_function():
     while(True):
-        letter = input("Choose letters to guess a word related to a color ")
+        letter = input("Choose letters to guess a word related to a color ").upper()
 
         special_characters = "!@#$%^&*()-+?_=,<>/"
         
-        # It invalidates inputs that are more than 1 character, is a digit, is a space, and is a special character
+        # It invalidates the inputs that are more than 1 character, is a digit, is a space, is a special character, and is a letter that was already used by the past player.
         if(len(letter)!= 1):
             print("ERROR input a letter, no more than one character")
             continue
@@ -125,13 +125,15 @@ def input_function():
         if  (letter in special_characters):
             print("ERROR input a letter, not a special character")
             continue
-
+        if (letter in my_List):
+            print ("ERROR input a letter, letter already used try another letter that you have not used")
+            continue
         my_List.append(letter)
         return letter
 
 # print(input_function())
 
-# This function that prints the word with the letters added by the player, if it was not a part of the letters in the word, then it stays the same. 
+# This function that prints the word in underscores. While the with the player adds characters, it changes the underscore into the letter added or if it is not a character in the word then if it was not a part of the letters in the word then it stays as an underscore. 
 def printword (color):
     Temp:str=""
     for letter in color:
@@ -144,8 +146,8 @@ def printword (color):
             break
     print (Temp)
 
+# Every time it runs, it detects if a letter that was not in the color name and adds +1 to the amount of errors.
 def getErrors(color):
-    # Every time it runs, it detects if a letter that was not in the color name and adds +1 to the amount of errors
     error = 0
 
     for letter in my_List:
@@ -154,8 +156,7 @@ def getErrors(color):
         
     return error
 
-# After the player answers the first letter, the game restarts the function that validates the letters, the one that prints the words, and if the steps are the same as the amount of errors
-
+# After the player answers the first letter, the game restarts all of the functions that are needed to run the game.
 while (True):
     color = getColor() 
     # error = getErrors(color)
@@ -163,12 +164,11 @@ while (True):
         error = getErrors(color)
         print (steps[error])
         printword(color)
-       
 
+        # When the variable "error" reach 6 it states to the player that it was a game over and restarts the game.
         if error == 6:
-            print("GAME OVER, you absolute buffoon the word was")
+            print(f"GAME OVER, you absolute buffoon the word was {color}")
             my_List = [""]
-            print (color)
             print("-------------------------------------------")
             break
 
