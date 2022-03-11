0497b985d59c922714f2dfb70c9a549735992ad0
CSI-Rafael-Bonilla <25-007@sanignacio.pr>
2022-03-11 09:49:25 -0400

    Snake

---
+++ Projects/Snake/Snake.py
@@ -0,0 +1,14 @@
+import pygame
+
+#initializing pygame
+pygame.init()
+
+#sets up a display mode that is on the parameter x 400 and y 300
+dis=pygame.display.set_mode((400,300))
+
+#updates the screen display
+pygame.display.update()
+
+#uninitializes everything
+pygame.quit()
+quit()
