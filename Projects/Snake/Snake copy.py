64dcc66d8fc8ad8e605aba352a8dd65be0c7d7cc
CSI-Rafael-Bonilla <25-007@sanignacio.pr>
2022-03-17 11:32:16 -0400

    Snake Copy

--- Projects/Snake/Snake copy.py
+++ Projects/Snake/Snake copy.py
@@ -7,10 +7,14 @@ pygame.init()
 dis=pygame.display.set_mode((400,300))
 
 #Updates the display
-pygame.display.update()
+#pygame.display.update()
 
 #Names the display after the set caption
 pygame.display.set_caption('Snake game by Edureka')
+
+#red, and blue are in the parameter "0, 0, 255", and "255,0,0" respectively
+blue=(0,0,255)
+red=(255,0,0)
 
 #It makes a look in which it when game_over is false it prints out all the actions that take place on the screen. 
 game_over=False
@@ -19,7 +23,11 @@ while not game_over:
 #When the X is pressed on the tab of the game, the code is terminated.
         if event.type==pygame.QUIT:
             game_over=True
- 
+
+    #Updates the display and draws blue        
+    pygame.draw.rect(dis,blue,[200,150,10,10])
+    pygame.display.update()
+
 #Used to uninitialize everything
 pygame.quit()
 quit()
