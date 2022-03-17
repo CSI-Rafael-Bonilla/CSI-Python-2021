778bc7010b80343db6045dc43c447f9e27838ea5
CSI-Rafael-Bonilla <25-007@sanignacio.pr>
2022-03-17 11:16:44 -0400

    Snake copy

---
+++ Projects/Snake/Snake copy.py
@@ -0,0 +1,15 @@
+import pygame
+
+#Initializes all of the imported Pygame modules.
+pygame.init()
+
+#Sets the display paramaters
+dis=pygame.display.set_mode((400,300))
+
+#updates the display
+pygame.display.update()
+
+
+#Used to uninitialize everything
+#pygame.quit()
+#quit()
