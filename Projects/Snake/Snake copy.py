f133c5be797cd69aa607778dd5763735b48e22dc
CSI-Rafael-Bonilla <25-007@sanignacio.pr>
2022-03-17 11:20:20 -0400

    Snake Copy

--- Projects/Snake/Snake copy.py
+++ Projects/Snake/Snake copy.py
@@ -6,10 +6,21 @@ pygame.init()
 #Sets the display paramaters
 dis=pygame.display.set_mode((400,300))
 
-#updates the display
+#Updates the display
 pygame.display.update()
 
+#Names the display after the set caption
+pygame.display.set_caption('Snake game by Edureka')
 
+#It makes a look in which it when game_over is false it prints out all the actions that take place on the screen. 
+game_over=False
+while not game_over:
+    for event in pygame.event.get():
+        print(event)   
+ 
 #Used to uninitialize everything
-#pygame.quit()
-#quit()
+pygame.quit()
+quit()
+
+
+
